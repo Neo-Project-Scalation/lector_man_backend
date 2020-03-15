6e35e5ea172051ed9e9e8bf6d9fb18747b53a890
CristianFonsec03 <cristian.lfs@gmail.com>
2020-03-15 18:03:51 -0500

    Added requirements.txt and return images

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
-from flask import Flask
-from flask_restful import Resource, Api
+from flask import Flask, send_from_directory
+from flask_restful import Resource, Api, abort
+from os import scandir, getcwd
+from os.path import abspath
 
 app = Flask(__name__)
 api = Api(app)
 
 
-class Hello(Resource):
-    def get(self, name):
-        return {"Hello": name}
+class GetMangaChapter(Resource):
+    @staticmethod
+    def get(manga, chapter):
+        images = []
+        for arch in scandir(getcwd() + '\\static\\mangas\\' + manga + '\\' + chapter):
+            images.append({"nombre": arch.name, "link": '/static/mangas/'+manga+'/'+chapter+'/'+arch.name})
+        return {"manga": manga, "chapter": chapter, "images": images}
 
 
-api.add_resource(Hello, '/hello/<name>')
+api.add_resource(GetMangaChapter, '/<manga>/<chapter>')
 
 if __name__ == '__main__':
     app.run()
